de62462fe8057f49d3c9c8b0214c27094d27489f
Hyperling <me@hyperling.com>
2024-03-30 13:45:03 -0700

    Builds successfully now, but still has 21 errors and 3 warnings. Very weird. Emulator will not run, will try a different laptop.

--- app/src/main/java/com/hyperling/tictactoe/MainActivity.kt
+++ app/src/main/java/com/hyperling/tictactoe/MainActivity.kt
@@ -9,11 +9,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hyperling.tictactoe.ui.theme.TicTacToeTheme
-import org.w3c.dom.Text
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,49 +19,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             TicTacToeTheme {
                 // A surface container using the 'background' color from the theme
-                /*Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }*/
-                Game()
+                Surface( modifier = Modifier.fillMaxSize()
+                    , color = MaterialTheme.colorScheme.background
+                ) {
+                    Game()
+                }
             }
         }
     }
 }
-/*
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicTacToeTheme {
-        Greeting("Android")
-    }
-}
-*/
 
 @Composable
 fun Game() {
     Text(text = "Let's play Tic-tac-Toe!")
 
     // Create 9 buttons for the grid. I wish this was XML...???!!!
-    lateinit var buttons: Array<Button>
+    //lateinit var buttons: Array<Button>
     for (i in 0..8) {
         val x = i % 3
         val y = i / 3
-        buttons.set(i, Button(
-            text = "Slot $x,$y"
-        )
+        //buttons.set(i,
+            Text(
+                text = "Slot $x,$y"
+            )
+        //)
     }
 }
 
 @Preview
 @Composable
 fun PreviewGame() {
-    Game("Yippee!")
+    Game()
 }
